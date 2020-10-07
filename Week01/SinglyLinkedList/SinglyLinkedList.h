ad2f0b02f3184c7e331bd40618dd425d7733031e
DenitsaStoianova <denitsa.stoianova@abv.bg>
2020-10-07 23:37:25 +0300

    Create SinglyLinkedList.h

---
+++ Week01/SinglyLinkedList/SinglyLinkedList.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class SinglyLinkedList
+{
+public:
+	// Конструктори
+	SinglyLinkedList();
+	SinglyLinkedList(const SinglyLinkedList& other);
+	SinglyLinkedList& operator=(const SinglyLinkedList& other);
+	~SinglyLinkedList();
+
+	// Добавяне на нов елемент в списъка
+	void pushBack(int data);
+	void pushFront(int data);
+
+	// Информативни методи
+	size_t getSize() const;
+	bool contains(int data) const;
+	void print() const;
+
+private:
+	struct Node // Представяне на възел от списъка
+	{
+		int data; // Стойност на възела
+		Node* next; // Указател, който пази връзка към следващия елемент в списъка
+
+		Node(int data, Node* next = nullptr)
+		{
+			this->data = data;
+			this->next = next;
+		}
+	};
+
+private:
+	Node* first; // Указател, който пази връзка към първия елемент в списъка
+	size_t size; // Пазим размера на списъка (ако е неоходимо)
+
+	// Помощни функции за работа с конструкторите
+	void copy(const SinglyLinkedList& other);
+	void clear();
+};
