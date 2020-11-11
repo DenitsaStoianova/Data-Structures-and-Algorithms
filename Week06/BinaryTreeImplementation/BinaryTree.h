b3d89e87964e6b1ec5c380da2b287b4e9f7765ce
DenitsaStoianova <denitsa.stoianova@abv.bg>
2020-11-11 23:59:44 +0200

    Create BinaryTree.h

---
+++ Week06/BinaryTreeImplementation/BinaryTree.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <string>
+
+class BinaryTree
+{
+public:
+	// Конструктори
+	BinaryTree();
+	BinaryTree(const BinaryTree& other);
+	BinaryTree& operator=(const BinaryTree& other);
+	~BinaryTree();
+
+	// Добавяне на нов елемент в дървото (използваме път, състоящ се от символни низове, за да определим къде точно да добавим подадения елемент)
+	// strPath е от вида: LLRLR - показва посоката, в която да се движим, за да стигнем до мястото, на което искаме да добавим елемент
+	void addEelement(int data, const std::string& strPath);
+
+	// Информативни функции
+	int getRootData() const;
+	int getCount() const;
+	bool isEmpty() const;
+
+private:
+	struct TreeNode // Предсавяне на възел от дървото
+	{
+		int data; // Стойност на възела
+		TreeNode* left; // Указател, който пази връзка към лявото поддърво на възела
+		TreeNode* right; // Указател, който пази връзка към дясното поддърво на възела
+
+		TreeNode(int data, TreeNode* left = nullptr, TreeNode* right = nullptr)
+		{
+			this->data = data;
+			this->left = left;
+			this->right = right;
+		}
+	};
+private:
+	// Указател към корена на дърото
+	TreeNode* root;
+
+	// Помощни функции за работа с конструкторите
+	TreeNode* copy(TreeNode* otherRoot);
+	void clear(TreeNode* &treeRoot);
+
+	// Помощна функция за намиране на броя на възлите в дървото
+	int getCountHelp(TreeNode* root) const;
+};
